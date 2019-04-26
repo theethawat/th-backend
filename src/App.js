d7c40e8224be3d678d9da9f7eaaee4c1f3347546
Theethawat <5910110150@psu.ac.th>
2019-04-27 00:23:32 +0700

    OAuth-With Google

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+let config = {
+  apiKey: "AIzaSyBhY4oZHA7hW20GZ61ullpx8ei_ixnzRjE",
+  authDomain: "theethawatapp.firebaseapp.com",
+  databaseURL: "https://theethawatapp.firebaseio.com",
+  projectId: "theethawatapp",
+  storageBucket: "theethawatapp.appspot.com",
+  messagingSenderId: "657980256737"
+};
+firebase.initializeApp(config);
+
+const signIn = () => {
+  let provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithRedirect(provider);
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Welcome to Admin Site </h1>
+        <button onClick={signIn}> Login </button>
     </div>
   );
 }
