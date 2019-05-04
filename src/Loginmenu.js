46bb2da5915cc1b2b381cca340621f60051f671e
Theethawat/Ubt <theethawat.s@outlook.co.th>
2019-05-04 09:27:06 +0700

    login Menu (Move to Ubuntu)

---
+++ src/Loginmenu.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import 'bulma/css/bulma.css'
+import firebase from './firebase'
+
+class Loginmenu extends Component {
+    //constructor like oop
+    constructor(props){
+        super(props)
+        this.state = {
+            email:'',
+            password:'',
+            currentUser:'',
+            message:''
+        }
+    }
+    //Method or function
+     componentDidMount(){
+         firebase.auth().onAuthStateChanged(user => {
+             if(user) {
+                 this.setState({
+                    currentUser:user
+                })
+             }
+         })
+     }
+    onChange = e =>{
+        const {name,value} = e.target
+        this.setState({
+            [name]:value
+        })
+    }
+
+    onSubmit = e => {
+        e.preventDefault()
+        this.setState({
+            email:this.email,
+            password:this.password
+        })
+      
+        firebase.auth().signInWithEmailAndPassword(this.state.email,this.state.password)
+            .then(response => {
+                this.setState({
+                    currentUser:response.user
+                })
+                console.log("Login Success")
+                console.log(this.state.currentUser)
+            })
+            .catch(error => {
+                this.setState({
+                    message:error.message
+                })
+                console.log("Login Error")
+                console.log(this.state.message)   
+                console.log(this.state.email) 
+            })
+
+    }
+
+
+    render() {
+        return (
+            <div className="container">
+                <br />
+                <div className="columns">
+                    <div className="column is-half">
+                        <h1 className="title is-3">Theethawat Backend Application</h1>
+                        <h4 className="subtitle is-4">Login</h4>
+                        <form onSubmit={this.onSubmit}  >
+                            <label className="label">Email</label>
+                            <input className="input " onChange={this.onChange} type="email" name="email"></input>
+                            <label className="label">Password</label>
+                            <input className="input " onChange={this.onChange} type="password" name="password"></input>
+                            <br />
+                            <button type="submit" className="button is-info">Login</button>
+                        </form>
+                    </div>
+                </div>
+                
+            </div>
+
+        )
+    }
+}
+export default Loginmenu
