03f99a7ff1f015968aed5d335899f35388521501
Theethawat/Ubt <theethawat.s@outlook.co.th>
2019-05-04 09:26:49 +0700

    Change to Ubuntu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React ,{Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Loginmenu from './Loginmenu';
-
+import ActiveLogin from "./ActiveLogin";
 class App extends Component {
-  render(){
-    return (
-    <div className="App">     
-        <Loginmenu/>
-    </div>
-  );
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      currentUser: '',
+      message: '',
+    }
   }
+  render() {
+    if (this.state.currentUser == null) {
+      return (
+        <div className="App">
+          <Loginmenu />
+        </div>
+      )
+    } else {
+      return (
+        <div className="App">
+          <ActiveLogin />
+        </div>
+      )
+    }
+  }
+
+
 }
+
 
 export default App;
